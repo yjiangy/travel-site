2ff2a6582d46433e9009beb9a15f55834bf4360a
yjiangy <jyy.eco01@gmail.com>
2020-08-31 17:57:30 -0500

    completed the reveal on scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,10 @@ import '../styles/styles.css'
 /*import Person from './modules/Person'
 import Animal from './modules/Animal';*/
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
 let mobileMenu = new MobileMenu();
 
